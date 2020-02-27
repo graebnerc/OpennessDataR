82dc871ef4c57f24c19e5b1f2d18ede89db078b0
Claudius Graebner <claudius@claudius-graebner.com>
2020-02-27 17:40:38 +0100

    Removed piging in openness_5y-csv

--- data-raw/openness_5y-csv.R
+++ data-raw/openness_5y-csv.R
@@ -1,15 +1,17 @@
 
 load("data/openness_1y.rda")
 
-full_data_5y <- openness_1y %>%
-  dplyr::group_by(ccode, period) %>%
-  dplyr::summarise_if(is.numeric, mean, na.rm = T) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(first_year = ifelse(Year == 1960, 1960,
-                                    ifelse(Year == 2016, 2016,
-                                           ifelse(Year == 1970, 1966, Year - 2)
-                                    )
-  ))
+full_data_5y <- dplyr::group_by(openness_1y, ccode, period)
+full_data_5y <- dplyr::summarise_if(full_data_5y, is.numeric, mean, na.rm = T)
+full_data_5y <- dplyr::ungroup(full_data_5y)
+full_data_5y <- dplyr::mutate(
+  full_data_5y,
+  first_year = ifelse(Year == 1960, 1960,
+                      ifelse(Year == 2016, 2016,
+                             ifelse(Year == 1970, 1966, Year - 2)
+                             )
+                      )
+  )
 
 data.table::fwrite(full_data_5y, "data-raw/openness_5y.csv")
 openness_5y <- full_data_5y
