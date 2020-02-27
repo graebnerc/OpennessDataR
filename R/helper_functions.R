c3aa6bc46c8bf67a544935ba4e140edea09eaad9
Claudius Graebner <claudius@claudius-graebner.com>
2020-02-27 17:52:02 +0100

    Update manuals, do not export test_uniqueness

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -10,7 +10,6 @@
 #' @return TRUE if data_table is unique, FALSE and a warning if it is not.
 #' @family update_dataset_helpers
 #' @import data.table
-#' @export
 test_uniqueness <- function(data_table, index_vars, print_pos=TRUE){
   data_table <- data.table::as.data.table(data_table)
   if (nrow(data_table)!=data.table::uniqueN(data_table, by = index_vars)){
